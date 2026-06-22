c0c1439b44625d4d159e860fc0775ff2ffd9d3e1
BEKO2210 <beko2210@example.com>
2026-06-22 17:48:21 +0200

    Deploy working Vercel serverless API without serverless-http wrapper

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
-const serverless = require('serverless-http');
 const handler = require('../rss-aggregator.js');
 
-module.exports = serverless(handler, {
-  binary: ['image/*', 'font/*'],
-});
+module.exports = handler;
